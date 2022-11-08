bb8149e49f6311bdc0152f3344445f95e2788219
João Vitor Soares Rocha <115377157+JoViSoRo@users.noreply.github.com>
2022-11-08 17:55:27 -0300

    correcao

--- exercicio.py
+++ exercicio.py
@@ -17,19 +17,19 @@ totalDeAlunos = numeroDeAlunos
 
 while numeroDeAlunos > 0:
     alunos.append(input('Nome do aluno: '))
-    nota1 = float(input('Nota da primeira AOP - * Notas entre 0.0 a 1.0 *: '))
+    nota1 = float(input('Nota da primeira AOP - * Nota entre 0.0 a 1.0 *: '))
     if nota1 > 1:
         print(f'A nota da primeira AOP, só pode valer até 1 ponto, e você colocou {nota1}')
         exit()
-    nota2 = float(input('Nota da segunda AOP - * Notas entre 0.0 a 2.0 *: '))
+    nota2 = float(input('Nota da segunda AOP - * Nota entre 0.0 a 2.0 *: '))
     if nota2 > 2:
         print(f'A nota da segunda AOP, só pode valer até 2 ponto, e você colocou {nota2}')
         exit()
-    nota3 = float(input('Nota da terceira AOP - * Notas entre 0.0 a 1.0 *: '))
+    nota3 = float(input('Nota da terceira AOP - * Nota entre 0.0 a 1.0 *: '))
     if nota3 > 1:
         print(f'A nota da terceira AOP, só pode valer até 1 ponto, e você colocou {nota3}')
         exit()
-    nota4 = float(input('Nota da prova regular(usando ponto): '))
+    nota4 = float(input('Nota da prova regular - * Nota entre 0.0 a 6.0 *: '))
     if nota4 > 6:
         print(f'A nota da prova 4, só pode valer até 6 ponto, e você colocou {nota4}')
         exit()
@@ -42,7 +42,7 @@ while nomeDosAlunos > 0:
     nomeDosAlunos = nomeDosAlunos - 1
     if medias[nomeDosAlunos] < 7:
         print('\033[033m='*50)
-        notarecuperacao = float(input(f'Aluno {alunos[nomeDosAlunos]} foi está quase reprovando, por favor, ensira a nota da prova de recuperação: '))
+        notarecuperacao = float(input(f'Aluno {alunos[nomeDosAlunos]} está quase reprovando, por favor, ensira a nota da prova de recuperação: '))
         
         if notarecuperacao >= 5:
             print('\033[032m='*50)
@@ -51,7 +51,7 @@ while nomeDosAlunos > 0:
 
         else:
             print('\033[031m='*50)
-            print(f'{alunos[nomeDosAlunos]} Reprovado(a) pois ficou com a média de {notarecuperacao}! ')
+            print(f'{alunos[nomeDosAlunos]} foi Reprovado(a) pois ficou com a média de {notarecuperacao}! ')
 
     else:
         print('\033[032m='*50)
